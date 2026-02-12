8887d316f8065922414985d80f9969a57eb680ba
Franco-sigm <francoherrera.30@outlook.com>
2026-02-12 20:44:57 -0300

    version depurada

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  // VITAL: Astro necesita esto para generar el sitemap y las URLs canónicas
+  site: 'https://surcode.cl',
+  integrations: [
+    tailwind(), 
+    sitemap()
+  ],
 });
